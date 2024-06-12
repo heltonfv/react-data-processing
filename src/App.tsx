ac33027a627fea0a47bebff3e142fa652e98c910
heltonfv <heltonapp@gmail.com>
2024-06-12 19:35:11 -0400

    create hook for option selected

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,18 @@ import datasource1 from './datasource/datasource1.json';
 
 function App() {
   const datasources = [
-    { name: "datasource1.json", "title": "Fonte de dados 1" },
-    { name: "datasource2.json", "title": "Fonte de dados 2" },
-    { name: "datasource3.json", "title": "Fonte de dados 3" },
-    { name: "datasource4.json", "title": "Fonte de dados 4" },
-    { name: "datasource5.json", "title": "Fonte de dados 5" }
+    { name: "datasource1.json", title: "Fonte de dados 1" },
+    { name: "datasource2.json", title: "Fonte de dados 2" },
+    { name: "datasource3.json", title: "Fonte de dados 3" },
+    { name: "datasource4.json", title: "Fonte de dados 4" },
+    { name: "datasource5.json", title: "Fonte de dados 5" }
   ];
+  
+  const [ selected, setSelected ] = useState(0);
+
+  const handleChange = (event:any) => {
+    setSelected(event.target.value);
+  }
 
   return (
     <Stack direction="column" spacing={2} justifyContent={"center"}>
@@ -49,18 +55,10 @@ function App() {
             <Paper sx={{padding: 1}} elevation={1}>
               <Grid container spacing={1}>
                 <Grid item xs={12} md={4}>
-                  <Select fullWidth label="Teste" size="small">
-                      <MenuItem>Teste</MenuItem>
-                  </Select>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                  <Select fullWidth label="Teste" size="small">
-                    <MenuItem>Teste</MenuItem>
-                  </Select>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                  <Select fullWidth label="Teste" size="small">
-                    <MenuItem>Teste</MenuItem>
+                  <Select onChange={handleChange} value={selected} fullWidth label="Teste" size="small">
+                    {datasources.map((item, index) => (
+                      <MenuItem value={index}>{item.title}</MenuItem>
+                    ))}
                   </Select>
                 </Grid>
               </Grid>
