a42cca1ab9c32a5bd5df5d00750d913fce435ca5
heltonfv <heltonapp@gmail.com>
2024-06-12 23:49:23 -0400

    create theme for project

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#b8b4b4'
+    }
+  },
+});
+
+export default theme;
