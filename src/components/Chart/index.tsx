223b58f748a52c7999b339ae6fbfbc0e40d140b0
heltonfv <heltonapp@gmail.com>
2024-06-14 00:32:10 -0400

    create filter

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -5,12 +5,26 @@ import { BarPlot } from '@mui/x-charts/BarChart';
 import { LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 
-export default function Chart(){
+export default function Chart({datasource}: any){
+    const pluck = (arr:any, key:any) => arr.map(i => i[key]);
+
+    // const data = ['group A', 'group B', 'group C'];
+    const data = pluck(datasource, 'nome')
+    const series = [
+        { data: pluck(datasource, 'id') },
+    ];
+
+    console.log(pluck(datasource, 'nome'));
+
     return (
         <Box height={400}>
             <BarChart
-                xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-                series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                xAxis={[
+                    {
+                        scaleType: 'band',
+                        data: data
+                    }]}
+                series={series}
             />
         </Box>
     );
