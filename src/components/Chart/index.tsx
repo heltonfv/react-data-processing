e4cd365b6dba776d24ac15c68dc71d9c33a28c2c
heltonfv <heltonapp@gmail.com>
2024-06-19 00:08:18 -0400

    add chart.js

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -5,18 +5,68 @@ import { BarPlot } from '@mui/x-charts/BarChart';
 import { LinePlot, MarkPlot } from '@mui/x-charts/LineChart';
 import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
 import _ from 'lodash';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );  
 
 export default function Chart({datasource}: any){
-    const group = _.map(datasource, 'visualizacao')
-    const data = _.map(datasource, 'soma')
+    const options = {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Chart.js Bar Chart - Stacked',
+          },
+        },
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true,
+          },
+        },
+      };
 
-    console.log(group)
+    const labels = _.map(datasource, 'ano');
+    let datasets:any = [];
+    const {ano, ...exceptYear} = datasource[0];
+    let newData = Object.keys(exceptYear)
+
+    newData.map((itens:any, index:any) => {
+        datasets.push({
+            data: datasource.map((iten:any, inde:any) => {
+                return iten[itens]
+            }),
+            label: itens
+        })
+    });
+
+    const data = {
+        labels,
+        datasets: datasets,
+      };
+      
+
     return (
         <Box height={400}>
-            <BarChart
-                xAxis={[{ scaleType: 'band', data: group }]}
-                series={[{data}]}
-            />
+            <Bar options={options} data={data} />
         </Box>
     );
 }
